e28eea5c7b2b26a0652dfe07dfd6e7e900c1a184
John Dowd and Pete Lazuran <john@>
2017-04-24 12:39:28 -0700

    added all four mathmatical formulas.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -5,4 +6,31 @@ var add = function(number1, number2) {
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
+alert(result);
+
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = sub(number1, number2);
+alert(result);
+
+var div = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = div(number1, number2);
+alert(result);
+
+var mult = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = mult(number1, number2);
 alert(result);
