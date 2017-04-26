86e8a77f229bed878a5ef6763644889e3e4743d7
Jared Luedtke and John Dowd <jared+john@>
2017-04-26 12:51:06 -0700

    add the user interface logic along with the the rest of the forms for calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+
+
+
+//////////////////////// Business Interface Logic ////////////////////////
 var add = function(number1, number2){
 	return number1 + number2;
 };
@@ -14,7 +18,38 @@ var divide = function(number1, number2){
 	return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number: "));
 
-alert(subtract(number1, number2));
+///////////////////////////// User Interface Logic //////////////////////////
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#output-add").text(result);
+	});
+
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#subtract1").val());
+		var number2 = parseInt($("#subtract2").val());
+		var result = subtract(number1, number2);
+		$("#output-subtract").text(result);
+	});
+
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#multiply1").val());
+		var number2 = parseInt($("#multiply2").val());
+		var result = multiply(number1, number2);
+		$("#output-multiply").text(result);
+	});
+
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#divide1").val());
+		var number2 = parseInt($("#divide2").val());
+		var result = divide(number1, number2);
+		$("#output-divide").text(result);
+	});
+});
